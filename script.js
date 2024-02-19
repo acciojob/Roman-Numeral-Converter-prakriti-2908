dafde9a49d46d8bb2705014b840d289e897b3044
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-19 14:31:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+   var romanNumeralMap = [
+      ['M',1000], 
+      ['D', 500], 
+      ['C', 100], 
+      ['L', 50], 
+      ['X', 10], 
+      ['V', 5], 
+      ['I', 1]
+   ];
+
+   var result = '';
+
+   for (var i = 0; i < romanNumeralMap.length; i++) {
+      var [roman, value] = romanNumeralMap[i];
+
+      while (num >= value) {
+         result += roman;
+         num -= value;
+      }
+   }
+
+   return result;
+}
+
+// function convertToRoman(num) {
+//   	const obj = {
+//       0:['M',1000], 
+//       1:['D', 500], 
+//       2:['C', 100], 
+//       3:['L', 50], 
+//       4:['X', 10], 
+//       5:['V', 5], 
+//       6:['I', 1]
+//     };
 
   //your code here
 
